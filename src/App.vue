0859e1ad58434bccace12070d30c6bf5d6ee51f0
dimakrol <dima.krol@mail.ru>
2017-01-15 23:45:30 +0200

    initial app

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <main-page></main-page>
+  </div>
+</template>
+
+<script>
+import MainPage from './components/MainPage'
+
+export default {
+  name: 'app',
+  components: {
+    MainPage
+  }
+}
+</script>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div>this is template body {{ msg }}</div>
+        <button @click="callLara">Hello</button>
+
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+        methods: {
+            callLara() {
+                this.$http.get('http://localhost:8000/people')
+                    .then(response => {
+                        console.log(response.body)
+                    })
+            }
+        }
+    }
+</script>
